42de2c8537601c6544b882c0c27859ccb46977a1
Yoshiaki Hidai <hidai.620@gmail.com>
2017-08-17 18:56:34 +0900

    Add Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,14 @@
-import { Component } from '@angular/core';
-import { Hero } from './Hero';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id:{{hero.id}}</label></div>
-    <div>
-      <label>name:</label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-  `,
-  styleUrls: ['./app.component.css']
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1, name: 'Windstorm'
-  };
 }
+
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from './hero.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes', component: HeroesComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
