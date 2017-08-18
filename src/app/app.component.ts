69a76ee2499ad91d93f168e3a68fbfb0cf7fc351
hidai620 <hidai.620@gmail.com>
2017-08-18 18:01:30 +0900

    Add http-mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,14 @@ import {Component} from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/heroes">Heroes</a>
+    </nav>
     <router-outlet></router-outlet>
-  `
+  `,
+  styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
   title = 'Tour of Heroes';
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,11 +1,30 @@
 import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
 import {HEROES} from './mock-heroes';
 import {Hero} from './hero';
+import {Observable} from 'rxjs/Observable';
 
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+  heroesUrl = 'api/heroes';
+
+  constructor(private http: Http) {
+  }
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  getHero(id: Number): Observable<Hero> {
+    return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
   }
 }
