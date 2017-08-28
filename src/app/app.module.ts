467d611daf470fe6b0ebd57d68a54f651d70adaf
hidai620 <hidai.620@gmail.com>
2017-08-28 17:59:19 +0900

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,13 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
+// debug
 import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import {InMemoryDataService} from './in-memory-data.service';
+
+// AngularMaterial
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MdInputModule, MdTableModule} from '@angular/material';
 
 import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard.component';
@@ -17,6 +22,11 @@ import {HeroSearchComponent} from './hero-search.component';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdInputModule,
+    MdTableModule,
     FormsModule,
     HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService),
